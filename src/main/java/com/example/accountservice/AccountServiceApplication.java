1f957487d4614e57f5752c13edef3dbe21858c07
cuong.pham4 <cuong.pham4@onemount.com>
2022-10-14 16:17:40 +0700

    init

--- src/main/java/com/example/accountservice/AccountServiceApplication.java
+++ src/main/java/com/example/accountservice/AccountServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class AccountServiceApplication {
@@ -10,4 +12,8 @@ public class AccountServiceApplication {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
 
+    @Bean
+    public WebClient createWebClient() {
+        return WebClient.create();
+    }
 }
